f3782083ecf311a41e4182626b4cb7909fe522ca
Christian Mahnke <cmahnke@gmail.com>
2024-03-30 22:40:38 +0100

    Updated installer

--- scripts/install-hdr-github.sh
+++ scripts/install-hdr-github.sh
@@ -7,6 +7,7 @@ git clone https://github.com/google/libultrahdr.git
 
 cd libultrahdr
 mkdir build
+cd build
 
-cmake -G "Unix Makefiles"  -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DUHDR_BUILD_TESTS=0 ../
+cmake -G "Unix Makefiles"  -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DUHDR_BUILD_TESTS=0 -DCMAKE_INSTALL_PREFIX=/usr/local ../
 make install
