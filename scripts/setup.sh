560844eaff4314b3c6faac74af7d01b50a64f195
Christian Mahnke <cmahnke@gmail.com>
2024-08-21 08:17:45 +0200

    wip

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,7 +10,8 @@ convert "Source Files/Images/preview-transparent.png" -channel RGB -negate stati
 
 #convert <input> -type Grayscale -gravity center -crop 3:2 <output>
 #PYTHON=`./themes/projektemacher-base/scripts/find-python3.sh` ./scripts/hdr.sh
-./scripts/hdr-docker.sh
+#./scripts/hdr-docker.sh
+./scripts/hdr.sh
 ./scripts/svgo.sh
 
 if [ -d ./scripts/post-build ] ; then
