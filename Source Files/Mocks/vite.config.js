29873d3ed700d1f85d259fa8c046862f728f0f26
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2025-08-17 08:37:10 +0200

    Minor updates

--- Source Files/Mocks/vite.config.js
+++ Source Files/Mocks/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
       mimeTypes,
       followSymlinks: true
     }),
-    eslint(),
+    eslint()
     //viteHtmlResolveAlias()
     //checker({ typescript: true })
   ],
