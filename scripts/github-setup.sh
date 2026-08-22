32553e1cf14e34085c5db5b752c62859b82b70a8
cmahnke <194820+cmahnke@users.noreply.github.com>
2026-08-22 12:45:56 +0200

    Remove setup-docker.sh from GitHub setup script
    Removed the setup-docker.sh script from the setup process.

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -7,7 +7,6 @@ CTX_PATH="$(dirname $(realpath $0))"
 $CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.11.1.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-media.sh
-$CTX_PATH/../themes/projektemacher-base/scripts/github/setup-docker.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-inkscape.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-fonts.sh
